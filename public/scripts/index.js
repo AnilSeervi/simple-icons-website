044545f486b5039df7fb28323251d1e195be32f5
Jorge Amado Soria Ramirez <darksoul.uci@gmail.com>
2021-12-08 11:15:37 -0600

    added all removed icons

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -12,10 +12,12 @@ import initModal from './modal.js';
 
 document.body.classList.remove('no-js');
 
-const storage = newStorage(localStorage);
-initColorScheme(document, storage);
-initCopyButtons(window, document, navigator);
-const orderingControls = initOrdering(document, storage);
-initSearch(window.history, document, orderingControls, domUtils);
-initDownloadType(document, storage);
-initModal(document, domUtils);
+if (!document.body.classList.contains('removed')) {
+  const storage = newStorage(localStorage);
+  initColorScheme(document, storage);
+  initCopyButtons(window, document, navigator);
+  const orderingControls = initOrdering(document, storage);
+  initSearch(window.history, document, orderingControls, domUtils);
+  initDownloadType(document, storage);
+  initModal(document, domUtils);
+}
